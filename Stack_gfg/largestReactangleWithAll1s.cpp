f063778278e8fa16d2de7754aff296ed8cf21dad
Aayush Dewangan <aayushdewangan20@gmail.com>
2023-06-24 15:45:03 +0530

    deque

--- Stack_gfg/largestReactangleWithAll1s.cpp
+++ Stack_gfg/largestReactangleWithAll1s.cpp
@@ -1,38 +1,38 @@
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
 
-int largestArea(int arr[]){
-    int res =0;
-    stack<int> s;
+// int largestArea(int arr[]){
+//     int res =0;
+//     stack<int> s;
 
-    for(int i =0; i<n; i++){
-        while(!s.empty() && arr[s.top()]>= arr[i]){
-            int tp = s.top();
-            s.pop();
-            int curr = arr[tp]*(s.empty() ? i : i - s.top() -1);
-            res = max(res, curr);
-        }
-        s.push(i);
-    }
-    while(!s.empty()){
-        int tp = s.top();
-        s.pop();
-        int curr = arr[tp]*(s.empty() ? n : n- s.top() -1);
-        res = max(res, curr);
-    }
-    return res;
+//     for(int i =0; i<n; i++){
+//         while(!s.empty() && arr[s.top()]>= arr[i]){
+//             int tp = s.top();
+//             s.pop();
+//             int curr = arr[tp]*(s.empty() ? i : i - s.top() -1);
+//             res = max(res, curr);
+//         }
+//         s.push(i);
+//     }
+//     while(!s.empty()){
+//         int tp = s.top();
+//         s.pop();
+//         int curr = arr[tp]*(s.empty() ? n : n- s.top() -1);
+//         res = max(res, curr);
+//     }
+//     return res;
 
-}
+// }
 
-int largestRectangle( int a[]c[])
+// int largestRectangle( int a[][C])
 
-int main(){
-    int A[][C] = {
-        { 0, 1, 1, 0 },
-        { 1, 1, 1, 1 },
-        { 1, 1, 1, 1 },
-        { 1, 1, 0, 0 },
-    };
-    cout<<largestRectangle(A);
+// int main(){
+//     int A[][C] = {
+//         { 0, 1, 1, 0 },
+//         { 1, 1, 1, 1 },
+//         { 1, 1, 1, 1 },
+//         { 1, 1, 0, 0 },
+//     };
+//     cout<<largestRectangle(A);
 
-}
+// }
