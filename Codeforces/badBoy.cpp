d82bd8d245110525ea0bbc01da882f40aa7d79d2
Aayush Dewangan <aayushdewangan20@gmail.com>
2023-06-27 01:27:27 +0530

    BFS

---
+++ Codeforces/badBoy.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long t;
+    cin>>t;
+    while(t--){
+        long n, m, i, j;
+        cin>>n>>m>>i>>j;
+        cout<<"1 "<<m<<" "<<n<< " 1\n"; //printf("1 %ld %ld 1\n", m, n);
+    }
+
+}
