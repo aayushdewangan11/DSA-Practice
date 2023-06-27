8f6a07e82c8f5012de911ae0c1899d7981e78593
Aayush Dewangan <aayushdewangan20@gmail.com>
2023-06-28 05:10:56 +0530

    updates

--- Graph_gfg/bfs_version1_source_given.cpp
+++ Graph_gfg/bfs_version1_source_given.cpp
@@ -6,15 +6,6 @@ void addEdge(vector<int> adj[], int u, int v){
     adj[v].push_back(u);
 }
 
-// void printGraph(vector<int> adj[], int v){
-//     for(int i =0; i<v; i++){
-//         for (int x : adj[i]){
-//             cout<<x<<" ";
-//         }
-//         cout<<endl;
-//     }
-// }
-
 void BFS(vector<int> adj[], int v, int s){
     bool visited[v+1];
     for(int i =0; i<v; i++){
