2759a960617f3a0fea33abaa8ca47a9f2c125386
Aayush Dewangan <aayushdewangan20@gmail.com>
2023-10-23 00:15:11 +0530

    CP

--- Codeforces/koxiaAndWhiteboards.cpp
+++ Codeforces/koxiaAndWhiteboards.cpp
@@ -1,3 +1,36 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     int t;
+//     cin >> t;
+//     while(t--) {
+//         int n, m;
+//         cin >> n >> m;
+
+//         vector<long long> a(n);
+//         vector<long long> b(m);
+
+//         for(int i = 0; i < n; i++) {
+//             cin >> a[i];
+//         }
+
+//         for(int i = 0; i < m; i++) {
+//             cin >> b[i];
+//         }
+
+//         for(int i = 0; i < m; i++){
+//             sort(a.begin(), a.end());
+//             a[0] = b[i]; 
+//         }
+
+//         long long sum = accumulate(a.begin(), a.end(), 0);
+//         cout<<sum<<endl;
+
+//     }
+// }
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -8,8 +41,8 @@ int main(){
         int n, m;
         cin >> n >> m;
 
-        vector<int> a(n);
-        vector<int> b(m);
+        vector<long long> a(n);
+        vector<long long> b(m);
 
         for(int i = 0; i < n; i++) {
             cin >> a[i];
@@ -19,19 +52,17 @@ int main(){
             cin >> b[i];
         }
 
-        sort(a.begin(), a.end(), greater<int>());
-        sort(b.begin(), b.end(), greater<int>());
+        sort(a.rbegin(), a.rend());
+        sort(b.rbegin(), b.rend());
 
         long long sum = 0;
-        int index = 0;
+        int j = 0;
 
-        for(int i = 0; i < n; i++) {
-            if(index < m && b[index] > a[i]) {
-                sum += b[index];
-                index++;
-            } else {
-                sum += a[i];
-            }
+        for(int i = 0; i < n && j < m && b[j] > a[i]; i++, j++){
+            sum += b[j];
+        }
+        for(int i = j; i < n; i++){
+            sum += a[i];
         }
 
         cout << sum << endl;
